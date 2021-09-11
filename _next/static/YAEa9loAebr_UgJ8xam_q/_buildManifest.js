94f0856fcd3acb0bfc4bdafda27f4ce55edddcaf
eels <eels@users.noreply.github.com>
2021-09-11 12:42:59 +0000

    deploy: e1cf52b19af78b01324fa33f22b1cbc8b575f300

---
+++ _next/static/YAEa9loAebr_UgJ8xam_q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/96928bbbb934fb3f3eca.css","static/chunks/pages/index-395e36325fbf31191086.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
