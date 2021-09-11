3e0fddda6a839c4b2b71e22c2d74818a80460cd0
Liam Howell <liam@liam.codes>
2021-09-11 16:00:05 +0100

    feat: add missing meta tags and favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,11 +7,19 @@ class MyDocument extends Document {
         <Head>
           <meta httpEquiv='x-ua-compatible' content='ie=edge' />
           <meta name='robots' content='index,follow,noimageindex' />
+          <meta name='application-name' content='Liam Howell' />
+          <meta name='theme-color' content='#f15d5d' />
           <link rel='canonical' href='https://liam.codes' />
           <link rel='author' href='/humans.txt' type='text/plain' />
           <link rel='manifest' href='/site.webmanifest' />
           <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
           <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+          <link
+            rel='apple-touch-icon'
+            type='image/png'
+            sizes='180x180'
+            href='apple-icon-180x180.png'
+          />
           <link rel='preconnect dns-prefetch' href='//emoji-css.afeld.me' />
           <link rel='preconnect dns-prefetch' href='//fonts.googleapis.com' />
           <link rel='preconnect dns-prefetch' href='//fonts.gstatic.com' />
