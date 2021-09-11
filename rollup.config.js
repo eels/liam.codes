816e2b85013355fea67d1d688de838145e33a704
Liam Howell <liam@liam.codes>
2021-09-11 18:35:42 +0100

    feat: add service-worker

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+const replace = require('@rollup/plugin-replace');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const { terser } = require('rollup-plugin-terser');
+
+module.exports = {
+  input: 'src/service-worker.ts',
+  output: {
+    dir: 'public/',
+    format: 'iife',
+    globals: {
+      'workbox-cacheable-response': 'workboxCacheableResponse',
+      'workbox-expiration': 'workboxExpiration',
+      'workbox-routing': 'workboxRouting',
+      'workbox-strategies': 'workboxStrategies',
+    },
+  },
+  plugins: [
+    replace({
+      preventAssignment: true,
+      values: {
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+    nodeResolve({ browser: true }),
+    terser(),
+  ],
+};
