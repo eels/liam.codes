2c02ed26cdaf234195be23866a54d93f5d608b4d
Liam Howell <liam@liam.codes>
2021-10-10 18:38:16 +0100

    chore: add explicit extensions to some dot files

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
+
   sassOptions: {
     includePaths: [path.join(__dirname, 'src')],
     prependData: `@import "${__dirname}/src/styles/shared";`,
