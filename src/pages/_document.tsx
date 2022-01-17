3985b13bf116372f1b297f4c1fe9eb3ed2a515b4
Liam Howell <liam@liam.codes>
2022-01-17 15:40:08 +0000

    fix: update missing apple touch icon file path

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,12 +14,7 @@ class MyDocument extends Document {
           <link rel='manifest' href='/site.webmanifest' />
           <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
           <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
-          <link
-            rel='apple-touch-icon'
-            type='image/png'
-            sizes='180x180'
-            href='apple-icon-180x180.png'
-          />
+          <link rel='apple-touch-icon' type='image/png' sizes='180x180' href='apple-touch.png' />
           <link rel='preconnect dns-prefetch' href='//emoji-css.afeld.me' />
           <link rel='preconnect dns-prefetch' href='//fonts.googleapis.com' />
           <link rel='preconnect dns-prefetch' href='//fonts.gstatic.com' />
